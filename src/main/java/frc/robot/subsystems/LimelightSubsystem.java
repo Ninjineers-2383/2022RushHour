344c01b36afab31567e6056b48189a0f3c5075e4
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2022-01-30 17:53:20 -0500

    Cleaned Up Code

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -12,82 +12,61 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class LimelightSubsystem extends SubsystemBase {
+
+  private final double TOLERANCE;
+  private final MedianFilter filteredX = new MedianFilter(5);
+  private final MedianFilter filteredY = new MedianFilter(1000);
+
+  private boolean targetValid = false;
+  private double targetX;
+  private double targetY;
+  
+
   /** Creates a new ExampleSubsystem. */
   public LimelightSubsystem() {
-    tolerance = Constants.LIMELIGHT_AIM_TOLERANCE;
+    TOLERANCE = Constants.LIMELIGHT_AIM_TOLERANCE;
   }
-
-  private boolean valid = false;
-  private double x;
-  private double y;
-  private double tolerance = 18;
-  private MedianFilter fx = new MedianFilter(5);
-  private MedianFilter fy = new MedianFilter(1000);
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
     NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-    NetworkTableEntry tv = table.getEntry("tv");
-    NetworkTableEntry tx = table.getEntry("tx");
-    NetworkTableEntry ty = table.getEntry("ty");
-    NetworkTableEntry ta = table.getEntry("ta");
+    NetworkTableEntry tableValidTarget = table.getEntry("tv");
+    NetworkTableEntry tableTargetX = table.getEntry("tx");
+    NetworkTableEntry tableTargetY = table.getEntry("ty");
 
     //read values periodically
-    x = fx.calculate(tx.getDouble(0.0));
-    valid = tv.getDouble(1) != 0;
-    y = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
-    
-    //the line right below doesn't work for some reason, so I'm using the y value above without the filter.
-    //y = fy.calculate(ty.getDouble(0.0));
-    double area = ta.getDouble(0.0);
+    targetX = filteredX.calculate(tableTargetX.getDouble(0));
+    targetValid = tableValidTarget.getDouble(1) != 0;
+    targetY = filteredY.calculate(tableTargetY.getDouble(0));
+
+
 
     //post to smart dashboard periodically
-    SmartDashboard.putNumber("LimelightX", x);
-    SmartDashboard.putNumber("LimelightY", y);
-    SmartDashboard.putNumber("LimelightArea", area);
-    SmartDashboard.putNumber("findDistance() value", findDistance());
+    SmartDashboard.putNumber("Target X", targetX);
+    SmartDashboard.putNumber("Target Y", targetY);
+    
   }
 
   public double getX() {
-    return x;
-  }
-
-  public double negativeX() {
-    if(x > 0) {
-      x = x * -1;
-      return x;
-    } else {
-      return x;
-    }
+    return targetX;
   }
 
   public double getY() {
-    return y;
-  }
-
-  public double findDistance() {
-    double t = Math.toRadians(Constants.LIMELIGHT_ANGLE + y);
-    //return t;
-    return (Constants.LIMELIGHT_HEIGHT_DIFFERENCE/Math.tan(t));
+    return targetY;
   }
 
   //returns direction to move ring turret; i.e. if right is returned, turret must turn right.
   public String direction() {
-    if (!valid) {
+    if (!targetValid) {
       return "Not Found";
     } else {
-      if (x < -tolerance) {
+      if (targetX < -TOLERANCE) {
         return "Left";
-      } else if (x > tolerance) {
+      } else if (targetX > TOLERANCE) {
         return "Right";
       }
       return "Locked On";
     }
-  }
-
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
   }
 }
