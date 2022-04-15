e840b965829a5914dcf6f7276fe8c2e1f4eec646
Henry LeCompte <henry@lunar-glass.com>
2022-04-15 17:35:11 -0400

    Auto Traversal fix and cleanup

--- src/main/java/frc/robot/commands/AutomatedCommands/TraversalClimbSequence.java
+++ src/main/java/frc/robot/commands/AutomatedCommands/TraversalClimbSequence.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.AutomatedCommands;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.IntakeCommand;
@@ -10,25 +11,17 @@ import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 
 public class TraversalClimbSequence extends SequentialCommandGroup {
-    private IntakeSubsystem intake;
-    private ClimberSubsystemNew climber;
-    private DrivetrainSubsystem drivetrain;
     private double climberPower = 0.75;
 
     public TraversalClimbSequence(DrivetrainSubsystem drivetrain, IntakeSubsystem intake, ClimberSubsystemNew climber) {
-
-        this.intake = intake;
-        this.climber = climber;
-        this.drivetrain = drivetrain;
-
         addRequirements(intake, climber, drivetrain);
-
-        drivetrain.toggleTippingDisabled();
 
         // Run path following command, then stop at the end.
         addCommands(
                 new SequentialCommandGroup(
-                        new ParallelCommandGroup( // hooooooking on mid bar
+                        new InstantCommand(
+                                drivetrain::toggleTippingDisabled),
+                        new ParallelCommandGroup( // hooking on mid bar
                                 new AutoForward(drivetrain, 0.75, 1, 0.5, 2),
                                 new IntakeCommand(intake, () -> 0, true, false),
                                 new TraversalClimbAutoCommand(climber, () -> climberPower, 250.0)),
